c92a7cb470f442743e2b0102d8aef307a7e770f6
lucas <1l9u9c8as@gmail.com>
2018-08-10 01:27:31 -0300

    Acrescentado comentários

--- trabalho1-CG.cpp
+++ trabalho1-CG.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-#include <stdlib.h>
 #include <GL/gl.h>
 #include <GL/freeglut.h>
-#include <math.h>
 #include <list>
-
-
 using namespace std;
 
-int memsafe = -10;
+const int WINDOWSIZE = 500;
+
 class Ponto {
     public:
         float x;
@@ -21,6 +18,7 @@ class Ponto {
 
 list<Ponto> pontos;
 
+//Calcula o ponto na metade da distância entre dois pontos
 Ponto pontoMedio(Ponto ponto1, Ponto ponto2){
     return Ponto((ponto1.x + ponto2.x)/2, (ponto1.y + ponto2.y)/2);
 }
@@ -29,13 +27,17 @@ void smoothPoints(){
     list<Ponto> result;
     
     list<Ponto>::iterator aux = pontos.begin();
+    //Acrescenta na lista o primeiro ponto, que é fixo
     result.push_back(*(pontos.begin()));
 
+    //Itera sobre a lista de pontos
     for (list<Ponto>::iterator it = pontos.begin();
          it != pontos.end(); ++it)
     {    
     
         list<Ponto>::iterator first, second, third;
+
+        //Pegamos referências para três pontos, que representam uma "quina" das retas
         first = second = third = it;
         advance(second, 1);
         
@@ -47,25 +49,34 @@ void smoothPoints(){
         if(third == pontos.end()){
             break;
         }
+
+        //Acrescenta os pontos necessários
         result.push_back(pontoMedio(pontoMedio(*first, *second), *second));
         result.push_back(pontoMedio(pontoMedio(*second, *third), *second));
     }
+
     advance(aux, 1);
+    //Checa se a lista possui apenas um elemento, acrescentando o último ponto da lista, que também é fixo, caso contrário
     if(aux != pontos.end()){
         aux = pontos.end();
         --aux;
         result.push_back(*aux);
     }
+    //Limpa a lista e atualiza com o resultado
+    pontos.clear();
+    pontos = move(result);
+}
 
-    pontos = result;
+void resetPoints(){
+    //Limpa a lista
+    pontos.clear();
 }
 
 void mouse (int button, int state, int givenX, int givenY)
 {
-    // Respond to mouse button presses.
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
-        pontos.push_back(Ponto((2.*givenX)/500 - 1., -(2.*givenY)/500 + 1.));
+        pontos.push_back(Ponto((2.*givenX)/WINDOWSIZE - 1., -(2.*givenY)/WINDOWSIZE + 1.));
         glutPostRedisplay();
     }
 }
@@ -74,16 +85,18 @@ void keyboard_cb(unsigned char key, int X, int Y)
 {
     switch (key)
     {
-        case 27:             // ESCAPE key
+        case 27:             
             exit (0);
             break;
-            
-        case 's':
 
-            if(memsafe < 6){
-                smoothPoints();
-            }
-            memsafe++;
+        //Tecla para chamar a suaviação das retas
+        case 's':
+            smoothPoints();
+            glutPostRedisplay();
+            break;
+        //Tecla para limpar os pontos
+        case 'r':
+            resetPoints();
             glutPostRedisplay();
             break;
     }
@@ -101,12 +114,12 @@ void display() {
 
     glBegin(GL_LINE_STRIP);
 
-    glColor3f(1.0, 0.0, 0.0);
+   
 
     for (list<Ponto>::iterator it = pontos.begin();
          it != pontos.end(); ++it)
     {      
-        glColor3f(1.0, 0.0, 0.0);
+        glColor3f(0.0, 0.0, 1.0);
         float px = it->x;
         float py = it->y;
         cout << "X: " << px << " Y: " << py << endl; 
@@ -121,7 +134,7 @@ void display() {
 int main(int argc, char **argv)
 {
     glutInit(&argc,argv);
-    glutInitWindowSize (500, 500);
+    glutInitWindowSize (WINDOWSIZE, WINDOWSIZE);
     glutCreateWindow("Pontos");
     glutKeyboardFunc(keyboard_cb);
     glutMouseFunc(mouse);
